fcc552702e90b7d783bac8b0129ceffec06f9160
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2023-04-08 11:12:18 +0530

    Implemented signin functionality

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from "react";
-// import { signIn } from "next-auth/react";
-// import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
+import { toast } from "react-hot-toast";
 
 import useLoginModal from "@/hooks/useLoginModal";
 import useRegisterModal from "@/hooks/useRegisterModal";
@@ -21,16 +21,16 @@ const LoginModal = () => {
     try {
       setIsLoading(true);
 
-      // await signIn("credentials", {
-      //   email,
-      //   password,
-      // });
+      await signIn("credentials", {
+        email,
+        password,
+      });
 
-      // toast.success("Logged in");
+      toast.success("Logged in");
 
       loginModal.onClose();
     } catch (error) {
-      // toast.error("Something went wrong");
+      toast.error("Something went wrong");
     } finally {
       setIsLoading(false);
     }
