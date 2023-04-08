f0255b54f15699b93039446059f5757d228401a9
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2023-04-08 11:15:20 +0530

    Added session provider and toaster component to main page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
 import Layout from "@/components/Layout";
 // import Modal from "@/components/Modal";
@@ -8,12 +10,13 @@ import RegisterModal from "@/components/modals/RegisterModal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
+      <Toaster />
       <RegisterModal />
       <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SessionProvider>
   );
 }
